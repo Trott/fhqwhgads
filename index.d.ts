ef764fa8ba35c357097c4378c77fd129ccd87603
Rich Trott <rtrott@gmail.com>
2022-04-08 22:06:37 -0700

    feat: add TypeScript types (#27)

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare const _default: {
+    fullName: string;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
--- test.js
+++ test.js
@@ -1,4 +1,3 @@
 import fhqwhgads from './index.js';
 import assert from 'node:assert';
-
 assert.deepStrictEqual(fhqwhgads, { fullName: 'fhqwhgadshgnsdhjsdbkhsdabkfabkveybvf' });
